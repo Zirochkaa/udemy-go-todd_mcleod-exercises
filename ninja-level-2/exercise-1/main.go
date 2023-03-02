3d74ef9f48ae6caf11fce3ce12629cd78164bcfb
oleh <lovemybeard@gmail.com>
2023-03-02 19:03:14 +0200

    Add all exesrices for ninja level 2

---
+++ ninja-level-2/exercise-1/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 42
+	fmt.Printf("%v\t%b\t%#x", n, n, n)
+}
---
+++ ninja-level-2/exercise-2/README.md
@@ -0,0 +1,8 @@
+# Exercise 2 for Ninja level 2
+
+---
+
+1. Run app:
+    ```shell
+    go run main.go
+    ```
